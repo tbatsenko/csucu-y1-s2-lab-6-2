b186e08e75b527d7951bfdf3da02563709d8b38d
tbatsenko <tbatsenko@gmail.com>
2017-03-13 19:48:27 +0200

    second task finished and tested correctly

--- line_intersect.py
+++ line_intersect.py
@@ -1,2 +1,37 @@
+# File: line_intersect.py
+# This module allows to check if two lines with given coords intersects.
+
+
+class Line(object):
+    # y = kx + b - line equation
+    def __init__(self, coords):
+        self.coords = coords
+        if coords[0] == coords[1]:
+            self.coords = None
+
+
+    def get_tg(self):
+        k = (self.coords[0][1] - self.coords[1][1])/(
+            self.coords[0][0] - self.coords[1][0])
+        return k
+
+    def get_b(self):
+        return self.coords[0][1] - self.get_tg()*self.coords[0][0]
+
+    def intersect(self, line):
+        if not self.coords:
+            return None
+        k1, k2 = self.get_tg(), line.get_tg()
+        b1, b2 = self.get_b(), line.get_b()
+        if k1 == k2 and b1 == b2:
+            return self
+        elif k1 == k2:
+            return None
+        else:
+            x = (b2-b1)/(k1-k2)
+            y = k1*x + b1
+            return x, y
+
+
 def line_intersect(line1, line2):
-    pass
+    return line1.intersect(line2)
--- test_line_intersect.py
+++ test_line_intersect.py
@@ -1,31 +1,52 @@
-"""
-Розробіть множину unittest тестів для функції line_intersect,
-яка призначена для пошуку точки перетину двох прямих.
- Прямі задані парою точок, через які вони проходять,
-  наприклад [(0.0, 0.0), (1.0, 3.0)].
-  Аргументами при виклику функції є дві прямі задані координатами цих точок.
-  Набір тестів повинен містити мінімум шість тестових випадків та враховувати
-  наступні особливості:
+# File: test_line_intersect.py
+# This module tests if module line_intersect.py works properly
 
-a.       Якщо прямі не перетинаються то функція line_intersect повертає None.
-
-b.       Якщо прямі перетинаються то функція line_intersect
-повертає точку перетину, наприклад (0.5, 0.75).
-
-c.       Якщо прямі співпадать то функція повинна повертати перший аргумент.
-
-
-При розробці тестів зверніть увагу (перечитайте help)
-по використанню методів setUp та tearDown класу TestCase.
-"""
-
-
+from line_intersect import Line
+from line_intersect import line_intersect
 import unittest
 
 
 class LineIntersectTestCase(unittest.TestCase):
+    counter = 1
+
     def setUp(self):
-        self.widget = Widget('The widget')
+        print("Setting uo the test {} ...".format(
+            LineIntersectTestCase.counter))
 
     def tearDown(self):
-        self.widget.dispose()
+        print("Test {} is finished".format(LineIntersectTestCase.counter))
+        LineIntersectTestCase.counter += 1
+
+    def test_no_intersection(self):
+        line_1 = Line([(0.0, 0.0), (1.0, 3.0)])
+        line_2 = Line([(0.0, 1.0), (1.0, 4.0)])
+        self.assertEqual(line_intersect(line_1, line_2), None)
+
+    def test_identic_lines(self):
+        line_1 = Line([(0.0, 0.0), (1.0, 3.0)])
+        line_2 = Line([(0.0, 0.0), (1.0, 3.0)])
+        self.assertEqual(line_intersect(line_1, line_2), line_1)
+
+    def test_invalid_line(self):
+        line_1 = Line([(0.0, 0.0), (0.0, 0.0)])
+        line_2 = Line([(0.0, 1.0), (2.0, 3.0)])
+        self.assertEqual(line_intersect(line_1, line_2), None)
+
+    def test_1(self):
+        line_1 = Line([(0.0, 0.0), (0.3, 0.0)])
+        line_2 = Line([(0.0, 1.0), (2.0, 3.0)])
+        self.assertEqual(line_intersect(line_1, line_2), (-1.0, 0.0))
+
+    def test_2(self):
+        line_1 = Line([(0.0, 0.0), (1.0, 3.0)])
+        line_2 = Line([(0.0, 1.0), (2.0, 3.0)])
+        self.assertEqual(line_intersect(line_1, line_2), (0.5, 1.5))
+
+    def test_3(self):
+        line_1 = Line([(0.0, 0.0), (5.0, 5.0)])
+        line_2 = Line([(2.0, 1.0), (6.0, 4.0)])
+        self.assertEqual(line_intersect(line_1, line_2), (-2.0, -2.0))
+
+
+if __name__ == '__main__':
+    unittest.main()
